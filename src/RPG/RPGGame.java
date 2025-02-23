2f78f7672cbced6cd5b1e8d0aeadd7722c6a7801
Jakubiou <jakubsramek2006@gmail.com>
2025-02-23 20:25:25 +0100

    added fullscreen

--- src/RPG/RPGGame.java
+++ src/RPG/RPGGame.java
@@ -1,16 +1,24 @@
 package RPG;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class RPGGame extends JFrame {
     private GamePanel gamePanel;
     private Player player;
 
     public RPGGame() {
+        GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+        if (gd.isFullScreenSupported()) {
+            setUndecorated(true);
+            gd.setFullScreenWindow(this);
+        } else {
+            System.err.println("Fullscreen not supported");
+            setSize(Toolkit.getDefaultToolkit().getScreenSize());
+        }
         player = new Player(Player.PANEL_WIDTH, Player.PANEL_HEIGHT, 100);
         gamePanel = new GamePanel(this, player);
         add(gamePanel);
-        pack();
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setVisible(true);
